92f880026f88d377b8e9aef769af1fbb212bb636
Kuan-Lun Wang <polyphonicared@gmail.com>
2025-07-31 14:31:30 +0800

    Refactor MonsterStatusManager to use pane_monster_webelement for element retrieval

--- src/hvbrowser/hv_battle_monster_status_manager.py
+++ src/hvbrowser/hv_battle_monster_status_manager.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webdriver import WebDriver, WebElement
 
 from .hv import HVDriver
 
@@ -28,24 +28,28 @@ class MonsterStatusManager:
         return self.hvdriver.driver
 
     @property
+    def pane_monster_webelement(self) -> WebElement:
+        """Returns the pane containing monster elements."""
+        return self.driver.find_element(By.ID, "pane_monster")
+
+    @property
+    def alive_monster_webelement(self) -> list[WebElement]:
+        """Returns the pane containing alive monster elements."""
+        return self.pane_monster_webelement.find_elements(
+            By.XPATH, self.ALIVE_MONSTER_XPATH
+        )
+
+    @property
     def alive_count(self) -> int:
         """Returns the number of monsters in the battle."""
-        elements = self.driver.find_elements(
-            By.XPATH,
-            self.ALIVE_MONSTER_XPATH,
-        )
-        return len(elements)
+        return len(self.alive_monster_webelement)
 
     @property
     def alive_monster_ids(self) -> list[int]:
         """Returns a list of IDs of alive monsters in the battle."""
-        elements = self.driver.find_elements(
-            By.XPATH,
-            self.ALIVE_MONSTER_XPATH,
-        )
         return [
             int(id_.removeprefix("mkey_"))
-            for el in elements
+            for el in self.alive_monster_webelement
             if (id_ := el.get_attribute("id")) is not None
         ]
 
@@ -53,7 +57,7 @@ class MonsterStatusManager:
     def alive_system_monster_ids(self) -> list[int]:
         """Returns a list of system monster IDs in the battle that have style attribute."""
         # 基於 ALIVE_MONSTER_XPATH 再加上有 style 設定的條件
-        elements = self.driver.find_elements(
+        elements = self.pane_monster_webelement.find_elements(
             By.XPATH,
             f"({self.ALIVE_MONSTER_XPATH})[@style]",
         )
@@ -71,7 +75,7 @@ class MonsterStatusManager:
             [f'@src="/y/e/{icon}" or @src="/isekai/y/e/{icon}"' for icon in icons]
         )
         xpath = f'//div[starts-with(@id, "mkey_")][.//img[{xpath_conditions}]]'
-        elements = self.driver.find_elements(By.XPATH, xpath)
+        elements = self.pane_monster_webelement.find_elements(By.XPATH, xpath)
         return [
             int(id_.removeprefix("mkey_"))
             for el in elements
@@ -83,7 +87,7 @@ class MonsterStatusManager:
         根據怪物名稱取得對應的 monster id（如 mkey_0 會回傳 0）。
         """
         xpath = f'//div[starts-with(@id, "mkey_")][.//div[text()="{name}"]]'
-        elements = self.driver.find_elements(By.XPATH, xpath)
+        elements = self.pane_monster_webelement.find_elements(By.XPATH, xpath)
         if elements:
             id_ = elements[0].get_attribute("id")
             if id_ and id_.startswith("mkey_"):
