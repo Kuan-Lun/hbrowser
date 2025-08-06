f192225c234b9fc7f8c03360a53d85e16f5f0037
Kuan-Lun Wang <polyphonicared@gmail.com>
2025-08-06 16:03:01 +0800

    Refactor LogProvider to accept BattleDashBoard instance and remove unused get method

--- src/hvbrowser/hv_battle_log.py
+++ src/hvbrowser/hv_battle_log.py
@@ -2,15 +2,16 @@ import re
 from collections import deque
 
 from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 
 from .hv import HVDriver
+from .hv_battle_dashboard import BattleDashBoard
 
 
 class LogProvider:
-    def __init__(self, driver: HVDriver) -> None:
+    def __init__(self, driver: HVDriver, battle_dashboard: BattleDashBoard) -> None:
         self.hvdriver: HVDriver = driver
+        self.battle_dashboard: BattleDashBoard = battle_dashboard
         self._reset_prev_lines()
         self.current_round = 0
         self.prev_round = 0
@@ -22,14 +23,6 @@ class LogProvider:
     @property
     def driver(self) -> WebDriver:
         return self.hvdriver.driver
-
-    def get(self) -> str:
-        result = self.hvdriver.driver.find_element(By.ID, "textlog").get_attribute(
-            "outerHTML"
-        )
-        if result is None:
-            return ""
-        return result
 
     def _parse_round_info(self, lines: list[str]) -> None:
         for line in lines:
@@ -43,8 +36,7 @@ class LogProvider:
                     self.total_round = int(match.group(2))
 
     def get_new_logs(self) -> list[str]:
-        html = self.get()
-        soup = BeautifulSoup(html, "html.parser")
+        soup = BeautifulSoup(self.battle_dashboard.log, "html.parser")
         lines = [td.text for td in soup.find_all("td", class_="tl")][-1::-1]
         new_lines = [line for line in lines if line not in self.prev_lines]
         self._parse_round_info(new_lines)
