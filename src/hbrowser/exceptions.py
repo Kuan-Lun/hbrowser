ebff62e5b6648ea846c4341dae9460c1786ec0d4
Kuan-Lun Wang <polyphonicared@gmail.com>
2025-12-15 21:10:13 +0800

    Enhance CAPTCHA handling by adding challenge detection and solving methods

--- src/hbrowser/exceptions.py
+++ src/hbrowser/exceptions.py
@@ -14,3 +14,21 @@ class InsufficientFundsException(Exception):
 
     def __str__(self):
         return self.message
+
+
+class CaptchaAPIKeyNotSetException(Exception):
+    def __init__(self, message="APIKEY_2CAPTCHA environment variable is not set."):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+
+class CaptchaSolveException(Exception):
+    def __init__(self, message="Failed to solve CAPTCHA challenge."):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
